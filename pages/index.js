d63f66f0beeaee57658677a3e2c29b9949e995c6
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2021-07-24 12:37:44 +0200

    Remove getServerSideProps function

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,8 @@
 import Layout from '../src/components/layout'
-import nookies, {parseCookies} from "nookies";
 
 export default function Page() {
   return (
     <Layout>
     </Layout>
   )
-}
-
-export async function getServerSideProps(context) {
-  const cookies = parseCookies(context)
-  const currentUser = "" !== cookies.USER_TOKEN ? cookies.USER_TOKEN : UserSession.getName();
-  nookies.set(context, 'USER_TOKEN', currentUser, {
-    maxAge: 60 * 5, // 60 seconds * 5 = 5 minutes
-    path: '/',
-  })
-  return {
-    props: {
-      githubUser: "XDS"
-    },
-  }
 }
